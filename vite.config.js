7512645dd9e8843b176636f9f967f84a4cdbba8e
gerard.esteve <gesteve.12@gmail.com>
2025-08-27 23:17:02 +0200

    main page is docs

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                bot: resolve(__dirname, 'bot/index.html'),
                 joc: resolve(__dirname, 'joc/index.html'),
             },
         },
